098f807eb3671aaccb6fe04b4e941504e5f7c1b7
fidman60 <benghazimofid@gmail.com>
2019-12-06 02:03:23 +0100

    change posts data from posts-list.component.html to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  posts = [{
+    title: 'Mon premier poste',
+    content: 'Nam solepud Siden, isdem impetu occurrere veloci. et signis prope ripam locatis ad manus comminus conseren',
+    loveIts: null
+  },
+    {
+      title: 'Mon deuxieme poste',
+      content: 'Nam solepud Siden,pe ripam locatis ad manus comminus conseren manus comminus conseren manus comminus conseren',
+      loveIts: null
+    },
+    {
+      title: 'Mon troisieme poste',
+      content: 'Nam solepud Siden, isdem impetu occurrere vel. ipam locatis ad manus comminus conseren',
+      loveIts: null
+    }];
 }
